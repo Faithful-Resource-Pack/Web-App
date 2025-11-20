bf314813f32fcbca5edec5909500983ed5cfcdaf
Evorp <75297863+3vorp@users.noreply.github.com>
2025-11-20 02:04:41 -0500

    new sidebar (see description)
    - the links are no longer all caps
    - added categories/collapsing
    - made the menu a bit less wide
    - marginally better light mode support

--- components/nav-sidebar.vue
+++ components/nav-sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-navigation-drawer v-model="drawerOpen" app width="320">
+	<v-navigation-drawer v-model="drawerOpen" app width="300">
 		<v-card :style="bannerStyles" class="rounded-t-0">
 			<div v-if="$root.isLoggedIn" class="py-2">
 				<v-card-title class="pl-2">
@@ -29,33 +29,54 @@
 			</div>
 		</v-card>
 
-		<div v-for="tab in tabs" :key="tab.label">
-			<v-list nav dense>
-				<v-list-item>
+		<!--
+			can't have multiple open tab groups with v-list so we manually add the classes
+			this is absolutely horrid but there's no other way to get the same behavior nicely
+		-->
+		<div class="v-list--dense v-list--nav mt-2">
+			<v-list-group
+				v-for="tab in tabs"
+				:key="tab.label"
+				color="inherit"
+				:value="initialTabsOpen[tab.label]"
+				@click="updateTabsOpen(tab.label)"
+			>
+				<template #activator>
 					<v-list-item-title class="uppercase-unsized">
 						{{ tab.labelText }}
 					</v-list-item-title>
-				</v-list-item>
-
-				<v-list-item
-					v-for="subtab in tab.subtabs"
-					:key="subtab.label"
-					class="uppercase-unsized"
-					link
-					:to="subtab.to"
-					:disabled="subtab.disabled"
-				>
-					<v-list-item-icon v-if="subtab.icon" class="mr-1">
-						<v-icon small>{{ subtab.icon }}</v-icon>
-					</v-list-item-icon>
-					<v-list-item-title>{{ subtab.labelText }}</v-list-item-title>
-
-					<v-list-item-action v-if="subtab.badge && badges[subtab.label]" class="nav-badge">
-						<span class="nav-badge-inner error white--text">{{ badges[subtab.label] }}</span>
-					</v-list-item-action>
-				</v-list-item>
-			</v-list>
-			<v-divider class="mx-2" />
+				</template>
+				<template #appendIcon>
+					<!-- regular is 24 and small is 16, both are a bit too extreme -->
+					<v-icon size="20" style="opacity: 80%">
+						{{ tabsOpen[tab.label] ? "mdi-chevron-up" : "mdi-chevron-right" }}
+					</v-icon>
+				</template>
+				<div class="v-list pb-4 pt-0">
+					<v-list-item
+						v-for="subtab in tab.subtabs"
+						:key="subtab.label"
+						link
+						:to="subtab.to"
+						:disabled="subtab.disabled"
+					>
+						<!-- for some reason the icon has a morbillion pixels of right padding -->
+						<v-list-item-icon v-if="subtab.icon" class="mr-0">
+							<v-icon small>{{ subtab.icon }}</v-icon>
+						</v-list-item-icon>
+						<v-list-item-content>
+							<v-list-item-title class="body-2">
+								{{ subtab.labelText || subtab.label.toTitleCase() }}
+							</v-list-item-title>
+						</v-list-item-content>
+						<v-list-item-action v-if="subtab.badge && badges[subtab.label]" class="nav-badge">
+							<span class="nav-badge-inner error white--text font-weight-black">
+								{{ badges[subtab.label] }}
+							</span>
+						</v-list-item-action>
+					</v-list-item>
+				</div>
+			</v-list-group>
 		</div>
 
 		<!-- Fix problem on firefox on mobile where bar disappears and fixed elements are hidden -->
@@ -64,6 +85,9 @@
 </template>
 
 <script>
+const OPEN_TAB_KEY = "menu_open_tabs";
+const MAX_USERNAME_DISPLAY_LENGTH = 15;
+
 export default {
 	name: "nav-sidebar",
 	props: {
@@ -82,15 +106,26 @@ export default {
 		},
 	},
 	data() {
+		const tabOpenJSON = localStorage.getItem(OPEN_TAB_KEY) || "{}";
 		return {
 			drawerOpen: false,
+			// updating tabsOpen when it's assigned to a value prop causes problems
+			initialTabsOpen: JSON.parse(tabOpenJSON),
+			// JSON.parse twice to get different references for mutation later
+			tabsOpen: JSON.parse(tabOpenJSON),
 		};
+	},
+	methods: {
+		updateTabsOpen(label) {
+			this.tabsOpen[label] = !this.tabsOpen[label];
+			localStorage.setItem(OPEN_TAB_KEY, JSON.stringify(this.tabsOpen));
+		},
 	},
 	computed: {
 		shortUsername() {
 			const username = this.$root.user.username;
-			if (username.length < 15) return username;
-			return `${username.slice(0, 15)}…`;
+			if (username.length < MAX_USERNAME_DISPLAY_LENGTH) return username;
+			return `${username.slice(0, MAX_USERNAME_DISPLAY_LENGTH)}…`;
 		},
 		bannerStyles() {
 			// MUST be done through css, using an image element does strange things with the padding
@@ -112,6 +147,22 @@ export default {
 		},
 		drawerOpen(newValue) {
 			this.$emit("input", newValue);
+		},
+		tabs(nv) {
+			const keys = nv.map((obj) => obj.label);
+
+			// we diff against the localstorage version since users can get logged out
+			// and their preference should be saved regardless
+			const keyDiff = keys.filter((k) => !Object.keys(this.tabsOpen).includes(k));
+			if (!keyDiff.length) return;
+
+			// properly new tabs are always open by default
+			for (const key of keyDiff) {
+				if (key === undefined) continue;
+				this.initialTabsOpen[key] = true;
+				this.tabsOpen[key] = true;
+			}
+			this.updateTabsOpen();
 		},
 	},
 };
