91a63e2541839de78f7fde6e356f3272583820bc
Evorp <75297863+3vorp@users.noreply.github.com>
2026-01-06 12:38:24 -1000

    chore(lint): update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,17 @@
+import { defineConfig } from "eslint/config";
 import pluginVue from "eslint-plugin-vue";
 
-export default [
-	...pluginVue.configs["flat/vue2-recommended"],
-	{
-		rules: {
-			"vue/html-self-closing": "off",
-			// site uses kebab-case for everything
-			"vue/component-definition-name-casing": "off",
-			"vue/order-in-components": "off",
-			"vue/attribute-hyphenation": "off",
-			"vue/v-on-event-hyphenation": "off",
-			// handled much better by prettier
-			"vue/html-indent": "off",
-			"vue/max-attributes-per-line": "off",
-			"vue/singleline-html-element-content-newline": "off",
-		},
+export default defineConfig(pluginVue.configs["flat/vue2-recommended"], {
+	rules: {
+		"vue/html-self-closing": "off",
+		// site uses kebab-case for everything
+		"vue/component-definition-name-casing": "off",
+		"vue/order-in-components": "off",
+		"vue/attribute-hyphenation": "off",
+		"vue/v-on-event-hyphenation": "off",
+		// handled much better by prettier
+		"vue/html-indent": "off",
+		"vue/max-attributes-per-line": "off",
+		"vue/singleline-html-element-content-newline": "off",
 	},
-];
+});
