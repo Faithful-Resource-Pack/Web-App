5ae8e77cd911daacf45eb5dc3652cc6e2a16bff7
Evorp <75297863+3vorp@users.noreply.github.com>
2026-06-26 21:11:09 -0700

    feat: versionSorter v4

--- helpers/versionSorter.js
+++ helpers/versionSorter.js
@@ -5,26 +5,36 @@
  */
 export default function versionSorter(a, b) {
 	const aStrings = a.split(".");
-	const aNumbers = aStrings.map((part) => Number(part));
+	const aNumbers = aStrings.map((aPart) => Number(aPart));
 	const bStrings = b.split(".");
-	const bNumbers = bStrings.map((part) => Number(part));
+	const bNumbers = aStrings.map((bPart) => Number(bPart));
 
 	// non-numbered versions go above everything else
-	if (aNumbers.every((a) => isNaN(a)) || bNumbers.every((b) => isNaN(b))) {
+	if (
+		aNumbers.every((aPart) => Number.isNaN(aPart)) ||
+		bNumbers.every((bPart) => Number.isNaN(bPart))
+	)
 		return a.localeCompare(b);
-	}
 
 	// compare only the safely accessible parts (1.17 vs 1.18.2 ignores the .2)
-	const upper = Math.min(aNumbers.length, bNumbers.length);
+	const upper = Math.min(aStrings.length, bStrings.length);
 
-	// compare each part by priority and immediately return if we find a difference
+	// compare each part by priority (a1.4 < b1.3 < 1.2 < 2.1)
 	for (let i = 0; i < upper; ++i) {
-		const result = aNumbers[i] - bNumbers[i];
-		// any version with a letter in it (e.g. b1.7.3) goes below everything else
-		if (isNaN(result)) return bStrings[i].localeCompare(aStrings[i]);
+		const aNaN = Number.isNaN(aNumbers[i]);
+		const bNaN = Number.isNaN(bNumbers[i]);
+
+		// if javascript had real pattern matching this would be so much nicer
+		let result;
+		if (aNaN && !bNaN) result = -1;
+		else if (!aNaN && bNaN) result = 1;
+		else if (aNaN && bNaN) result = aStrings[i].localeCompare(bStrings[i]);
+		else result = aNumbers[i] - bNumbers[i];
+
+		// return difference if exists, proceed to minor/patch otherwise
 		if (result !== 0) return result;
 	}
 
-	// each part in the safe boundary is the same, try for differing length (1.17 vs 1.17.1)
+	// each part in the safe boundary is the same, check length (1.17 < 1.17.1)
 	return aNumbers.length - bNumbers.length;
 }
